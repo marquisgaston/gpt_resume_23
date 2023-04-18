aa0e5b55397b41db7489aa540ab47cc9077489f6
Marquis Gaston <marquisgbusiness@gmail.com>
2023-04-18 01:47:44 -0400

    starting on css styles for results

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,38 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import ResultsBox from "./resultsBox";
 import searchData from "../searchData";
 
-const SearchBox = ({}) => {
+const SearchBox = () => {
   const [inputValue, setInputValue] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   const handleInputChange = (event) => {
     const value = event.target.value;
     setInputValue(value);
-    if (value === "") {
-      // No search input
-      setSearchResults([]);
-    } else {
-      // Search all fields of each item for the input value
-      setSearchResults(
-        searchData.projects
-          .concat(searchData.websites, searchData.githubLinks, searchData.youtubeVideos)
-          .filter((item) => {
-            for (const key in item) {
-              if (Object.prototype.hasOwnProperty.call(item, key) && typeof item[key] === "string" && item[key].toLowerCase().includes(value.toLowerCase())) {
-                return true;
-              }
-            }
-            return false;
-          })
-      );
-    }
+    setCurrentPage(1);
   };
 
+  const filteredResults = useMemo(() => {
+    if (inputValue === "") {
+      return [];
+    }
+
+    const allResults = searchData.projects
+      .concat(searchData.websites, searchData.githubLinks, searchData.youtubeVideos)
+      .filter((item) => {
+        for (const key in item) {
+          if (
+            Object.prototype.hasOwnProperty.call(item, key) &&
+            typeof item[key] === "string" &&
+            item[key].toLowerCase().includes(inputValue.toLowerCase())
+          ) {
+            return true;
+          }
+        }
+        return false;
+      });
+
+    return allResults;
+  }, [inputValue]);
+
+  const maxPage = Math.ceil(filteredResults.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredResults.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="search-box-container">
-      <input type="search" placeholder="Search" onChange={handleInputChange} value={inputValue} />
-      <ResultsBox searchResults={searchResults} inputValue={inputValue} />
+    <div className={`search-box-container ${!inputValue ? "center" : ""}`}>
+      {!inputValue && <h1 className="search-box-title">What Can I Do For You?</h1>}
+      <input className="search-box"
+        type="search"
+        placeholder="Search"
+        onChange={handleInputChange}
+        value={inputValue}
+      />
+      {currentItems.length > 0 && <ResultsBox searchResults={currentItems} />}
+      {filteredResults.length > itemsPerPage && (
+        <div className="pagination-container">
+          {Array.from({ length: maxPage }).map((_, i) => (
+            <button
+              key={i}
+              className={`pagination-button ${
+                currentPage === i + 1 ? "active" : ""
+              }`}
+              onClick={() => paginate(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/resultsBox.js
+++ src/components/resultsBox.js
@@ -1,44 +1,34 @@
-import React from 'react';
-import generateSearchResults from '../searchResults';
+import React from "react";
 
-const ResultsBox = ({ searchResults, inputValue }) => {
-  const extraResults = generateSearchResults(20);
+const ResultsBox = ({ searchResults }) => {
+  if (searchResults.length === 0) {
+    return <div className="results-box-container">No results found</div>;
+  }
 
-  const filterResults = (results, term) => {
-    return results.filter((result) => {
-      const values = Object.values(result);
-      return values.some((value) => {
-        if (typeof value === 'string') {
-          return value.toLowerCase().includes(term.toLowerCase());
-        }
-        return false;
-      });
-    });
+  const renderItem = (item) => {
+    return (
+      <div key={item.id} className="result-item">
+        {Object.entries(item).map(([key, value]) => {
+          if (key === 'link') {
+            return (
+              <a key={key} href={value} className="item-link">
+                {value}
+              </a>
+            );
+          }
+          return (
+            <div key={key} className={`item-${key}`}>
+              {value}
+            </div>
+          );
+        })}
+      </div>
+    );
   };
 
-  const filteredResults = inputValue !== '' ? filterResults(searchResults.concat(extraResults), inputValue) : [];
-
   return (
-    <div className="results-box">
-      <div>
-        {filteredResults.length > 0 ? (
-          filteredResults.map((result, index) => (
-            <div key={index} className="result-item">
-              <strong>{result.title || result.name}</strong>
-              <br />
-              {result.description || ''}
-              <br />
-              {result.link || ''}
-              <br />
-              {result.img || ''}
-              <br />
-              {result.url || ''}
-            </div>
-          ))
-        ) : (
-          inputValue !== '' && <p>No results found.</p>
-        )}
-      </div>
+    <div className="results-box-container">
+     {searchResults.map((item) => renderItem(item))}
     </div>
   );
 };
